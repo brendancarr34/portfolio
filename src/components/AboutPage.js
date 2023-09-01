4fa0a84a33c34ee5525781c5ee9e26ba128551dc
Brendan Carr <brendan.c34@gmail.com>
2023-09-01 00:39:09 -0700

    add pages

---
+++ src/components/AboutPage.js
@@ -0,0 +1,13 @@
+// src/components/AboutPage.js
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      {/* Add content for the about page */}
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Add content for the home page */}
+    </div>
+  );
+}
+
+export default HomePage;
