e8c8e67ff92d3d839f9458c9415f0325b2111119
Brendan Carr <42624540+brendancarr34@users.noreply.github.com>
2023-09-05 01:03:53 -0700

    test mobile update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          brendan test mobile
         </a>
       </header>
     </div>
